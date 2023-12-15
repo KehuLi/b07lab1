2d988fc3e91806a1c6b5350b6bba190af915faba
KehuLi <kehu.li@mail.utoronto.ca>
2023-12-15 18:05:09 -0500

    --> updated add(); --> added helper two functions;

--- Driver.java
+++ Driver.java
@@ -1,28 +1,25 @@
+package lab2;
+
 public class Driver {
-    public static void main(String [] args){
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
-        // double[] c1 = {6, 0, 0, 5};
-        // System.out.println(c1);
-        // double[] c2 = {0, -2, 0, 0, -9};
-        // System.out.println(c2);
-        // Polynomial a = new Polynomial(c1);
-        // Polynomial b = new Polynomial(c2);
-        // Polynomial p = a.add(b);
-        // int l = p.coefficients.length;
-        //System.out.println(p.coefficients.length);
-        // for (int i=0; i<l; i++){
-        //     System.out.println(p.coefficients[i]);
-        // }
+    public static void main(String[] args){
+        double []  c1 = {6,2,3};
+        int []     e1 = {0,2,4};
+        Polynomial p1 = new Polynomial(c1, e1);
+
+        double []  c2 = {2,-2,7};
+        int []     e2 = {0, 2, 3};
+        Polynomial p2 = new Polynomial(c2, e2);
+
+        System.out.println("testing add");
+        Polynomial test1 = p1.add(p2);
+        for (int i = 0; i < test1.coefficients.length; i++){
+            System.out.print(test1.coefficients[i] + " ");
+        System.out.println();
+        }
+        for (int i = 0; i < test1.exponents.length; i++){
+            System.out.print(test1.exponents[i] + " ");
+        System.out.println();
+        }
+
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,51 +1,64 @@
+package lab2;
+// Replace the array representing the coefficients by two arrays: one representing the non-
+// zero coefficients (of type double) and another one representing the corresponding
+// exponents (of type int). For example, the polynomial 6 − 2𝑥 + 5𝑥3 would be represented
+// using the arrays [6, -2, 5] and [0, 1, 3]
 public class Polynomial{
     double[] coefficients;
+    int[] exponents;
     
     // ii - constructor - no argument constructor
     public Polynomial(){
         this.coefficients = new double[1];
+        this.exponents = new int[1];
     }
     // iii - constructor - an array of double as an argument
-    public Polynomial(double[] coefficients){
+    public Polynomial(double[] coefficients, int[] exponents){
         int length = coefficients.length;
+        int length_e = exponents.length;
         this.coefficients = new double[length];
-        for (int i = 0; i < length; i++){        
+        this.exponents = new int[length_e];
+        for (int i = 0; i < length; i++){
             this.coefficients[i] = coefficients[i];
-        }//s
+        }
+        for (int j = 0; j < length_e; j++){
+            this.exponents[j] = exponents[j];
+        }
     }
+
     // iv - method - add
-    public Polynomial add(Polynomial polynomial){
-        int length1 = this.coefficients.length;
-        int length2 = polynomial.coefficients.length;
-        int max = length1;
-        int min = length2;
+    public Polynomial add(Polynomial that){
+        
+        // S1 - guard clause
+        if (this.exponents.length == 0)
+            return new Polynomial(that.coefficients, that.exponents);
+        if (that.exponents.length == 0)
+            return new Polynomial(this.coefficients, this.exponents);
+        
+        // S2 - create coefficients array from 0 to the largest exponents
+        double[] coe_raw = this.addCoefficients(that);
 
-        // System.out.println("Hello");
+        // S3 - num of non-zero coefficients
+        int len = 0;
+        for (int i = 0; i < roe_raw.length; i++) {
+            if (coe_raw[i] == 0)
+            len++;
+        }
 
-        if (length1 <= length2){
-            max = length2;
-            min = length1;
-            int i = 0;
-            Polynomial P = new Polynomial(polynomial.coefficients);
-            while (i < min){
-                P.coefficients[i] = this.coefficients[i] + polynomial.coefficients[i];
-                i++;
+        // S4 - create new polynomial
+        double[] new_coe = new double[len];
+        int[] new_exp = new int[len];
+
+        int j = 0;
+        for (int i = 0; i < coe_raw.length; i++) {
+            if (coe_raw[i] != 0) {
+                new_coe[j] = coe_raw[i];
+                new_exp[j] = i;
+                j++;
             }
-            
-            return P;
         }
-        else {
-            max = length1;
-            min = length2;
-            int i = 0;
-            Polynomial P = new Polynomial(this.coefficients);
-            while (i < min){
-                P.coefficients[i] = this.coefficients[i] + polynomial.coefficients[i];
-                i++;
-            }
-            
-            return P;
-        }
+
+        return new Polynomial(new_coe, new_exp);
     }
 
 
@@ -68,7 +81,29 @@ public class Polynomial{
         }
         return false;
     }
-}
 
+
+    // Helper function to find the largest exponents value
+    public double maxExp(Polynomial that){
+        int max_exp = Math.max(this.exponents[this.exponents.length - 1], that.exponents[that.exponents.length - 1]);
+        return max_exp;
+    }
+
+
+    // Helper function to add coefficients of two polynomials
+    public double[] addCoefficients(Polynomial that) {
+        int max_exp = this.maxExp(that);
+        double[] coe = new double[max_exp];
+
+        for (int i = 0; i < this.coefficients.length; i++)
+            coe[this.exponents[i]] += this.coefficients[i];
+
+        for (int i = 0; i < that.coefficients.length; i++)
+            coe[that.exponents[i]] += that.coefficients[i];
+
+        return coe;
+    }
+
+}
 
 
